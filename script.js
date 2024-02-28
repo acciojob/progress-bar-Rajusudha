fb98609f3617bc9caa81771dde061bdb3fa477c1
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-02-28 19:50:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ const circles = document.querySelectorAll('.circle');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+
+    let currentIndex = 0;
+
+    function updateProgress(direction) {
+      currentIndex += direction;
+      updateButtons();
+
+      circles.forEach((circle, index) => {
+        circle.classList.toggle('active', index < currentIndex);
+      });
+    }
+
+    function updateButtons() {
+      prevButton.disabled = currentIndex === 0;
+      nextButton.disabled = currentIndex === circles.length;
+    }
